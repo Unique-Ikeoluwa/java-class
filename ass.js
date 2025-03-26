4d4befad81639df9aac32d473a6792a3255db16a
Unique-Ikeoluwa <ikeoluwaadegbite@gmail.com>
2025-03-26 23:22:00 +0100

    assignment

---
+++ ass.js
@@ -0,0 +1,85 @@
+// for +
+let pt= 400
+let jp= 31
+console.log(pt+jp)
+// for +=
+let pm = 54
+let am = 41
+pm+=am
+console.log(pm)
+// for -
+let tota= 400
+let male= 31
+console.log(tota+male)
+// for -=
+let total = 200
+let female = 110
+total-=110
+console.log(total)
+// for *
+let mt= 400
+let jh= 31
+let f9= 65
+console.log(mt*jh*f9)
+// for *=
+let tem = 51
+let sef =80
+let pay= 95
+tem*=sef*pay
+console.log(tem)
+// for /
+let k = 7
+let t= 54
+console.log(t/k)
+// for /=
+let mn = 7
+let hf= 56
+let ok = 0
+mn/=ok/hf
+console.log(mn)
+// for %
+let y= 4
+let h= 9
+console.log(h%y)
+// for %=
+let p= 99
+let l= 109
+let g= 11
+p%=l%g
+console.log(p)
+// for **
+let o= 2
+let x= 3
+console.log(o**x)
+// for **=
+let a = 3
+let b = 2
+let c = 1
+a**=b**c
+console.log(a)
+// for adding and subtracting one(1)
+let ft= 6
+let gt = 8
+console.log((++ft)*(--gt))
+console.log((ft++)*(gt--))
+
+let git= 12
+let fit = 10
+console.log((++git)*(fit--))
+console.log((git)*(fit))
+// using ` and $
+console.log(`my value is ${fit}`)
+// working with comparison operators
+let ab = 56
+let bc = 31
+let fl = "31"
+let e = 0
+// for ==(equal)
+console.log(ab==bc)
+console.log(1==((ab+bc)**e))
+// for !=(not equal)
+console.log(ab!=fl)
+// for ===(strictly equal)
+console.log(bc===fl)
+// !==(strictly not equal)
+console.log(bc!==fl)
